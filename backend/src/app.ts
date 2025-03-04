c7c2e6c3242fe0082a8ff1c853715c0ee10f5fcf
Russell Johnson-Bade <russell.johnson-bade@corrivium.com>
2025-03-04 11:17:10 +1100

    Import crypto module
    (And run formatting, sorry!)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,54 +1,56 @@
+import { randomUUID } from "node:crypto";
+
 export type TodoId = string;
 export type Todo = {
   id: TodoId;
-  summary: string,
-  done: boolean,
+  summary: string;
+  done: boolean;
 };
 export type TodoList = Todo[];
-export type TodoAdd = Omit<Todo, 'id'>;
+export type TodoAdd = Omit<Todo, "id">;
 export type TodoUpdate = Partial<TodoAdd>;
 
 export type TodoApp = {
-  list: () => TodoList,
-  add: (todo: TodoAdd) => Todo,
-  update: (id: TodoId, update: TodoUpdate) => Todo,
-  remove: (id: TodoId) => void  
-}
+  list: () => TodoList;
+  add: (todo: TodoAdd) => Todo;
+  update: (id: TodoId, update: TodoUpdate) => Todo;
+  remove: (id: TodoId) => void;
+};
 
 export type TodoStore = {
   set: (id: string, todo: Todo) => void;
   get: (id: string) => Todo | undefined;
   list: () => Todo[];
   delete: (id: string) => void;
-}
+};
 
 type CreateTodoAppOpts = {
-  store: TodoStore
-}
+  store: TodoStore;
+};
 
 export function createTodoApp(opts: CreateTodoAppOpts): TodoApp {
-  const {store} = opts;
+  const { store } = opts;
 
   return {
     list() {
-      return store.list()
+      return store.list();
     },
     add(todoDetails) {
       const todo = {
         id: generateTodoId(),
-        ...todoDetails
+        ...todoDetails,
       };
       store.set(todo.id, todo);
       return todo;
     },
     update(id, updateDetails) {
       const existingTodo = store.get(id);
-      if(!existingTodo) {
+      if (!existingTodo) {
         throw new ClientError(`Todo with id '${id}' does not exist`);
       }
       const updatedTodo = {
         ...existingTodo,
-        ...updateDetails
+        ...updateDetails,
       };
       store.set(id, updatedTodo);
       return updatedTodo;
@@ -57,7 +59,7 @@ export function createTodoApp(opts: CreateTodoAppOpts): TodoApp {
       store.delete(id);
       return;
     },
-  }
+  };
 }
 
 export class ClientError extends Error {
@@ -68,6 +70,5 @@ export class ClientError extends Error {
 }
 
 function generateTodoId() {
-  return crypto.randomUUID()
+  return randomUUID();
 }
-
